b5439d52a9a576c9922cb0ace2316e62c50d1fc0
Dennis Hickox <frozzel@me.com>
2024-11-26 13:38:14 -0500

    avatar build init

--- src/App.js
+++ src/App.js
@@ -105,6 +105,7 @@ import MainLandingPage from "MainLandingPage.js";
 import ThankYouPage from "ThankYouPage.js";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VR from "pages/VR";
 
 export default function App() {
   // If you want to disable the animation just use the disabled `prop` like below on your page's component
@@ -123,6 +124,8 @@ export default function App() {
           <Route path="/components/:type/:name" element={<ComponentRenderer />} />
           <Route path="/thank-you" element={<ThankYouPage />} />
           <Route path="/" element={<MainLandingPage />} />
+          <Route path="*" element={<MainLandingPage />} />
+          <Route path="/VR-Demo" element={<VR />} />
         </Routes>
       </Router>
     </>
