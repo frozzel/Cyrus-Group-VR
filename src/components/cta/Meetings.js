93638fe3af5ee1053caf0299a8346014aeef7bda
Dennis Hickox <frozzel@me.com>
2024-12-11 14:25:46 -0500

    fixed meet fail safe

--- src/components/cta/Meetings.js
+++ src/components/cta/Meetings.js
@@ -16,12 +16,18 @@ const MeetingsEmbed = () => {
     };
   }, []);
 
-  return (
-    <div
-      className="meetings-iframe-container"
-      data-src="https://cyrusgroupinnovations.com/meetings/dennis-hickox?embed=true"
-    ></div>
-  );
+  return (<>
+      {/* <div className="meetings-iframe-container" data-src="https://cyrusgroupinnovations.com/meetings/dennis-hickox?embed=true"></div> */}
+    <div className="meetings-iframe-container" data-src="https://meetings.hubspot.com/dennis-hickox?embed=true"></div>
+    {/* <script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"></script> */}
+    <iframe 
+        title="Cyrus Group Innovations - Meetings"
+        style={{width: '100%', height: '100vh'}}
+        src="https://cyrusgroupinnovations.com/meetings/dennis-hickox?embed=true"
+        />
+    
+    </>);
 };
+
 
 export default MeetingsEmbed;
